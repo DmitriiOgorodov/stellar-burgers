4dcbacde7e5a3445208bb72aaaa866500a7cc675
dmitrii ogorodov <dmitrii.ogorodov@yandex.ru>
2025-11-13 00:17:37 +0300

    Добавлены роуты ConstructorPage и Feed в App component.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,17 @@
-import { ConstructorPage } from '@pages';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
+import { ConstructorPage, Feed } from '@pages';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+    </Routes>
   </div>
 );
 
