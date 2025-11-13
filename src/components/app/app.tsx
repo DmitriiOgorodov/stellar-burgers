2719f2e1036062fb25fd4f85872a281403872a19
dmitrii ogorodov <dmitrii.ogorodov@yandex.ru>
2025-11-13 06:26:26 +0300

    Добавлены роуты OrderInfo и ProfileOrders в App component.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,7 +2,7 @@ import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader, ProtectedRoute } from '@components';
+import { AppHeader, ProtectedRoute, OrderInfo } from '@components';
 import {
   ConstructorPage,
   Feed,
@@ -21,6 +21,7 @@ const App = () => (
     <Routes>
       <Route path='/' element={<ConstructorPage />} />
       <Route path='/feed' element={<Feed />} />
+      <Route path='/feed/:number' element={<OrderInfo />} />
       <Route
         path='/login'
         element={
@@ -29,6 +30,55 @@ const App = () => (
           </ProtectedRoute>
         }
       />
+      <Route
+        path='/register'
+        element={
+          <ProtectedRoute>
+            <Register />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/forgot-password'
+        element={
+          <ProtectedRoute>
+            <ForgotPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/reset-password'
+        element={
+          <ProtectedRoute>
+            <ResetPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/profile'
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/profile/orders'
+        element={
+          <ProtectedRoute>
+            <ProfileOrders />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/profile/orders:number'
+        element={
+          <ProtectedRoute>
+            <ProfileOrders />
+          </ProtectedRoute>
+        }
+      />
+      <Route path='*' element={<NotFound404 />} />
     </Routes>
   </div>
 );
