5f143ff91522672b9d898db2f36f5b58b0bb5cf1
dmitrii ogorodov <dmitrii.ogorodov@yandex.ru>
2025-11-16 10:49:45 +0300

    Подключен `useSelector` для получения ингредиентов из Redux store в компоненте `BurgerIngredients`.

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,14 +1,19 @@
-import { useState, useRef, useEffect, FC } from 'react';
+import { useState, useRef, useEffect, FC, useMemo } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 import { TTabMode } from '@utils-types';
 import { BurgerIngredientsUI } from '../ui/burger-ingredients';
+import { useSelector } from '../../services/store';
 
 export const BurgerIngredients: FC = () => {
-  /** TODO: взять переменные из стора */
-  const buns = [];
-  const mains = [];
-  const sauces = [];
+  const items = useSelector((state) => state.ingredients.items);
+
+  const { buns, mains, sauces } = useMemo(() => {
+    const buns = items.filter((i) => i.type === 'bun');
+    const mains = items.filter((i) => i.type === 'main');
+    const sauces = items.filter((i) => i.type === 'sauce');
+    return { buns, mains, sauces };
+  }, [items]);
 
   const [currentTab, setCurrentTab] = useState<TTabMode>('bun');
   const titleBunRef = useRef<HTMLHeadingElement>(null);
@@ -46,8 +51,6 @@ export const BurgerIngredients: FC = () => {
     if (tab === 'sauce')
       titleSaucesRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
-
-  return null;
 
   return (
     <BurgerIngredientsUI
