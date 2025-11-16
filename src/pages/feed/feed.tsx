7c8cfbf6702e0d8bbf47783e0469b7ce63490747
dmitrii ogorodov <dmitrii.ogorodov@yandex.ru>
2025-11-16 16:33:52 +0300

    Подключен `useSelector` и `useDispatch` для управления состоянием ленты заказов в компонентах Feed и FeedInfo, добавлена логика загрузки данных с использованием `fetchFeeds`.

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,15 +1,22 @@
 import { Preloader } from '@ui';
 import { FeedUI } from '@ui-pages';
-import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useDispatch, useSelector } from '../../services/store';
+import { fetchFeeds } from '../../services/slices/feedsSlice';
 
 export const Feed: FC = () => {
-  /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const dispatch = useDispatch();
+  const { orders, loading } = useSelector((state) => state.feeds);
 
-  if (!orders.length) {
+  useEffect(() => {
+    if (!orders.length) dispatch(fetchFeeds());
+  }, [dispatch]);
+
+  if (loading && !orders.length) {
     return <Preloader />;
   }
 
-  return <FeedUI orders={orders} handleGetFeeds={() => {}} />;
+  return (
+    <FeedUI orders={orders} handleGetFeeds={() => dispatch(fetchFeeds())} />
+  );
 };
