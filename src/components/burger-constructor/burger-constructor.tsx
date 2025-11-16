4e0d077a22a570b43d3bfdc85b082b4fadee4041
dmitrii ogorodov <dmitrii.ogorodov@yandex.ru>
2025-11-16 16:32:17 +0300

    Подключены действия Redux для управления ингредиентами в BurgerIngredient, BurgerConstructor и BurgerConstructorElement.

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,24 +1,44 @@
 import { FC, useMemo } from 'react';
 import { TConstructorIngredient } from '@utils-types';
 import { BurgerConstructorUI } from '@ui';
+import { useSelector, useDispatch } from '../../services/store';
+import { placeOrder, clearOrder } from '../../services/slices/orderSlice';
+import { clearConstructor } from '../../services/slices/constructorSlice';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const BurgerConstructor: FC = () => {
-  /** TODO: взять переменные constructorItems, orderRequest и orderModalData из стора */
-  const constructorItems = {
-    bun: {
-      price: 0
-    },
-    ingredients: []
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  // Защитный доступ к слайсу конструктора на случай раннего рендера/HMR
+  const constructorItems = useSelector(
+    (state) => state.burgerConstructor?.items
+  ) ?? {
+    bun: null,
+    ingredients: [] as TConstructorIngredient[]
   };
-
-  const orderRequest = false;
-
-  const orderModalData = null;
+  const { loading: orderRequest, modalData: orderModalData } = useSelector(
+    (state) => state.order
+  );
+  const user = useSelector((state) => state.user.user);
 
   const onOrderClick = () => {
     if (!constructorItems.bun || orderRequest) return;
+    if (!user) {
+      navigate('/login', { replace: true, state: { from: location } });
+      return;
+    }
+    const ingredientIds: string[] = [
+      constructorItems.bun._id,
+      ...constructorItems.ingredients.map((i) => i._id),
+      constructorItems.bun._id
+    ];
+    dispatch(placeOrder(ingredientIds));
   };
-  const closeOrderModal = () => {};
+  const closeOrderModal = () => {
+    dispatch(clearOrder());
+    dispatch(clearConstructor());
+  };
 
   const price = useMemo(
     () =>
@@ -29,8 +49,6 @@ export const BurgerConstructor: FC = () => {
       ),
     [constructorItems]
   );
-
-  // return null;
 
   return (
     <BurgerConstructorUI
