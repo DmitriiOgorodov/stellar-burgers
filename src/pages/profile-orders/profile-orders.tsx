c3d4fbfb9e3371fd83e40ece2488ae8fd0bdc804
dmitrii ogorodov <dmitrii.ogorodov@yandex.ru>
2025-11-16 16:38:23 +0300

    Подключены `useSelector` и `useDispatch` для управления состоянием заказов и пользователя в компонентах `ProfileOrders`, `Profile` и `ProfileMenu`; добавлена логика загрузки заказов, обновления данных пользователя и выхода из системы.

--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -1,10 +1,18 @@
 import { ProfileOrdersUI } from '@ui-pages';
-import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useDispatch, useSelector } from '../../services/store';
+import { fetchProfileOrders } from '../../services/slices/profileOrdersSlice';
+import { Preloader } from '@ui';
 
 export const ProfileOrders: FC = () => {
-  /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const dispatch = useDispatch();
+  const { orders, loading } = useSelector((state) => state.profileOrders);
+
+  useEffect(() => {
+    dispatch(fetchProfileOrders());
+  }, [dispatch]);
+
+  if (loading && !orders.length) return <Preloader />;
 
   return <ProfileOrdersUI orders={orders} />;
 };
